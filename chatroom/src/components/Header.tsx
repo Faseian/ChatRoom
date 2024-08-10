ed34884023233599fa171d13b78deeb326f58dae
faseian <nab4nkert@outlook.com>
2024-08-10 00:19:12 -0400

    Restarting from scratch

---
+++ chatroom/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            <div>Home</div>
+            <input type="search" name="search" id="search-bar" />
+        </div>
+    )
+}
+
+export default Header;
