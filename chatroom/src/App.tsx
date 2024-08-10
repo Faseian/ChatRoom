7d0e8f655dfeb8837ec0464e45ba4e1011bff727
faseian <nab4nkert@outlook.com>
2024-08-10 11:24:31 -0400

    Cleanup

--- chatroom/src/App.tsx
+++ chatroom/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import Home from "./components/Home"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import './index.css'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Home/>
+      <Footer/>
     </div>
   )
 }
--- chatroom/src/components/Home.tsx
+++ chatroom/src/components/Home.tsx
@@ -1,12 +1,9 @@
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-
 function Home() {
     return (
         <>
-            <Header></Header>
-            <Footer></Footer>
+            <div>
+                Hello
+            </div>
         </>
     )
 }
